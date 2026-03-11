a91ed4eb90724e17fc541e9ed69a01759b2187f1
softworkz <softworkz@hotmail.com>
2026-03-11 17:09:15 +0100

    [Emby] avcodec/mpegts: Add AC-4 decoder

--- libavcodec/parsers.c
+++ libavcodec/parsers.c
@@ -23,6 +23,7 @@
 extern const AVCodecParser ff_aac_parser;
 extern const AVCodecParser ff_aac_latm_parser;
 extern const AVCodecParser ff_ac3_parser;
+extern const AVCodecParser ff_ac4_parser;
 extern const AVCodecParser ff_adx_parser;
 extern const AVCodecParser ff_amr_parser;
 extern const AVCodecParser ff_apv_parser;
