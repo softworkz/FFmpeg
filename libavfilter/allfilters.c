tcoza/FFmpeg2

avfilter/graphicsub2text: Add new graphicsub2text filter (OCR)
Signed-off-by: softworkz <softworkz@hotmail.com>

libavfilter/allfilters.c
@@ -571,6 +571,7 @@ extern const AVFilter ff_avsrc_movie;
 
 /* subtitle filters */
 extern const AVFilter ff_sf_censor;
+extern const AVFilter ff_sf_graphicsub2text;
 extern const AVFilter ff_sf_showspeaker;
 extern const AVFilter ff_sf_splitcc;
 extern const AVFilter ff_sf_stripstyles;

softworkz/FFmpeg

libavfilter/allfilters.c
@@ -627,6 +627,7 @@ extern const FFFilter ff_avsrc_movie;
 
 /* subtitle filters */
 extern const FFFilter ff_sf_censor;
+extern const FFFilter ff_sf_graphicsub2text;
 extern const FFFilter ff_sf_showspeaker;
 extern const FFFilter ff_sf_splitcc;
 extern const FFFilter ff_sf_stripstyles;