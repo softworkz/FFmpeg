tcoza/FFmpeg2

avfilter/textmod: Add textmod, censor and show_speaker filters
- textmod {S -> S)
  Modify subtitle text in a number of ways

- censor {S -> S)
  Censor subtitles using a word list

- show_speaker {S -> S)
  Prepend speaker names from ASS subtitles to the visible text lines

Signed-off-by: softworkz <softworkz@hotmail.com>

libavfilter/allfilters.c
@@ -569,6 +569,11 @@ extern const AVFilter ff_avsrc_avsynctest;
 extern const AVFilter ff_avsrc_amovie;
 extern const AVFilter ff_avsrc_movie;
 
+/* subtitle filters */
+extern const AVFilter ff_sf_censor;
+extern const AVFilter ff_sf_showspeaker;
+extern const AVFilter ff_sf_textmod;
+
 /* those filters are part of public or internal API,
  * they are formatted to not be found by the grep
  * as they are manually added again (due to their 'names'

softworkz/FFmpeg

libavfilter/allfilters.c
@@ -625,6 +625,11 @@ extern const FFFilter ff_avsrc_avsynctest;
 extern const FFFilter ff_avsrc_amovie;
 extern const FFFilter ff_avsrc_movie;
 
+/* subtitle filters */
+extern const FFFilter ff_sf_censor;
+extern const FFFilter ff_sf_showspeaker;
+extern const FFFilter ff_sf_textmod;
+
 /* those filters are part of public or internal API,
  * they are formatted to not be found by the grep
  * as they are manually added again (due to their 'names'